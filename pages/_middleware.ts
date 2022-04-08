2e273004abb354b37344da49f2ec6fcfbc150dd7
qt-coder <chuchenkhang@gmail.com>
2022-04-08 18:44:19 +0800

    Auth protection middleware

---
+++ pages/_middleware.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest } from "next"
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+
+export async function middleware(req: NextApiRequest) {
+    // return early if url isn't supposed to be protected
+    if (!req.url.includes("/protected")) {
+        return NextResponse.next()
+    }
+
+    const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+    if (!session) return NextResponse.redirect("/api/auth/signin")
+
+    return NextResponse.next()
+}
